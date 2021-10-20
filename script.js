aa5d23f62073619ac7646ea0fd2b87217eb064c5
AripovJr <jasurbek_aripov@gmail.ru>
2021-10-21 00:42:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
-const secondsEl = document.querySelector('.seconds')
+const secondEl = document.querySelector('.seconds')
+const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggleEl = document.querySelector('.toggle') 
 
@@ -21,18 +22,30 @@ toggleEl.addEventListener('click', (e)=>{
 })
 
 function setTime(){
-    const time = new Date()
+    const time = new Date();
     const month = time.getMonth()
+    const date = time.getDate()
     const day = time.getDay()
     const hours = time.getHours()
-    const hoursForClock = hours % 12
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+    
+
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes<10?`0${minutes}`:minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
-setTime()
-
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+setTime()
+
+setInterval(setTime, 1000)
